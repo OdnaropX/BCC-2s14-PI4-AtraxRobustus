e98119331dbd3f3dc499b294fadd9372e0cb82e6
Gabriel <colecionador.gabriel@gmail.com>
2014-10-04 02:41:14 -0300

    Add spider for mangaupdates

--- src/crawler/crawler/spiders/mangaupdates.py
+++ src/crawler/crawler/spiders/mangaupdates.py
@@ -2,6 +2,7 @@
 import scrapy
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
+import re
 
 class MangaUpdatesSpider(CrawlSpider):
 		name = "mangaupdates"
@@ -12,33 +13,60 @@ class MangaUpdatesSpider(CrawlSpider):
 		"http://www.mangaupdates.com/genres.html",
 		"http://www.mangaupdates.com/series.html?page=1&",
 		"http://www.mangaupdates.com/categories.html?page=1&"]
+		
 		rules = (
-		Rule(LinkExtractor(allow=['genres\.html?']), callback='parse_genres'),
-		Rule(LinkExtractor(allow=('series\.html'), deny=('letter', 'orderby', 'filter', 'category', 'act')), 'parse_series'),
-		#Rule(LinkExtractor(allow=['publishers\.html']), 'parse_publishers'),
-		#Rule(LinkExtractor(allow=['categories\.html']), 'parse_categories'),
-		#Rule(LinkExtractor(allow=['groups\.html']), 'parse_groups'),
-		#Rule(LinkExtractor(allow=['authors\.html']), 'parse_authors'),
+		Rule(LinkExtractor(allow=('series\.html\?page=[0-9]{1,}'), deny=('letter', 'orderby', 'filter', 'categories', 'act'))),
+		Rule(LinkExtractor(allow=('publishers\.html\?page=[0-9]{1,}'))),
+		Rule(LinkExtractor(allow=['categories\.html?page=[0-9]{1,}'])),
+		Rule(LinkExtractor(allow=['groups\.html\?page=[0-9]{1,}'])),
+		Rule(LinkExtractor(allow=['authors\.html\?page=[0-9]{1,}'])),
+		Rule(LinkExtractor(allow=('id=', 'genres\.html')), callback='parse', follow=False)
 		)
 		
-		#rules = (
-        # Extract links matching 'category.php' (but not matching 'subsection.php')
-        # and follow links from them (since no callback means follow=True by default).
-        #Rule(LinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
-
-        # Extract links matching 'item.php' and parse them with the spider's method parse_item
-        #Rule(LinkExtractor(allow=('item\.php', )), callback='parse_item'),
-		#)
+		pattern_series = re.compile(ur'series\.html\?id=[0-9]{1,}')
+		pattern_publishers = re.compile(ur'publishers\.html\?id=[0-9]{1,}')
+		pattern_groups = re.compile(ur'groups\.html\?id=[0-9]{1,}')
+		pattern_categories = re.compile(ur'categories\.html\?id=[0-9]{1,}')
+		pattern_authors = re.compile(ur'authors\.html\?id=[0-9]{1,}')
+		pattern_genres = re.compile(ur'genres\.html')
 	
 		def parse(self, response):
-			filename = response.url.replace("?", "-");
-			filename = filename.replace("http://", "");
-			filename = filename.replace("/", "-");
-			#with open(filename, 'wb') as f:
-				#f.write(response.body)
+			if(re.search(self.pattern_series, response.url) != None):
+				#Parse Series.
+				self.parse_series(self, response)
+				
+			elif(re.search(self.pattern_groups, response.url) != None):
+				#Parse Groups.
+				self.parse_groups(self, response)
+				
+			elif(re.search(self.pattern_genres, response.url) != None):
+				#Parse Genres.
+				self.parse_genres(self, response)
+				
+			elif(re.search(self.pattern_categories, response.url) != None):
+				#Parse Categories.
+				self.parse_categories(self, response)
+				
+			elif(re.search(self.pattern_authors, response.url) != None):
+				#Parse Authors.
+				self.parse_authors(self, response)
+				
+			elif(re.search(self.pattern_publishers, response.url) != None):
+				#Parse Publisher.
+				self.parse_publishers(self, response)
+			
+			#filename = response.url.replace("?", "-");
+			#filename = filename.replace("http://", "");
+			#filename = filename.replace("/", "-");
+			#with open("urls.txt", 'a') as f:
+			#	f.write(response.url + "\n")
+			
 		
+		#def parse_start_url(self, response):
+		#	list(self.parse_links(response))
 		
 		def parse_series(self, response):
+			
 			#self.log('Hi, this is an item page! %s' % response.url)
 			# item = scrapy.Item()
 			# item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
@@ -51,21 +79,21 @@ class MangaUpdatesSpider(CrawlSpider):
 			filename = filename.replace("/", "-");
 			
 			with open(filename, 'wb') as f:
-				f.write(response.body)
+				#f.write(response.body)
 				f.write("2")
 
-		#def parse_groups(self, response):
-		
+		def parse_groups(self, response):
+			
 
-		#def parse_authors(self, response):
-		
-		
-		#def parse_publishers(self, response):
+		def parse_authors(self, response):
 			
+		
+		def parse_publishers(self, response):
 			
-		#def parse_categories(self, response):
+		
+		def parse_categories(self, response):
 		
 		def parse_genres(self, response):
-			with open("teste.txt", 'w') as f:
+			with open("teste.txt", 'a') as f:
 				f.write("2")
 				
