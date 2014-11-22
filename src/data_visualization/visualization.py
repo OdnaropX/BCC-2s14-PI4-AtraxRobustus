e3d55c6c9662f9a8d934d81ad4c57027d2272ff4
Gabriel <colecionador.gabriel@gmail.com>
2014-11-22 18:02:44 -0200

    Begin data visualization

---
+++ src/data_visualization/visualization.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import numpy as np
+import math
+from random import Random
+
+class Visualization:
+
+	def find_avaliable_space(self, scene, size_x, size_y):
+		pass
+	
+	def cloud_words(self, words = []):
+		pass
+	
+	def spiral(self, collections = []):
+		pass
+	
